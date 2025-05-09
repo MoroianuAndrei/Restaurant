910f262ebbe2d6e4997b3e35a932c7eb20730823
MoroianuAndrei <madalin.moroianu@student.unitbv.ro>
2025-05-09 11:35:57 +0300

    Cleaning project

--- Restaurant/obj/Debug/net8.0-windows/Restaurant.AssemblyInfo.cs
+++ Restaurant/obj/Debug/net8.0-windows/Restaurant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef68158bfc07de6a115c89a595e97507b918fe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62d791b382954504842c122df1bd7ba92d4f30cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
